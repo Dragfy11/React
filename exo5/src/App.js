0557421fe659e12c0d8e8a38ac99aa686ba9ec7c
Kadir <kadir.poyraz@outlook.be>
2021-01-14 13:47:30 +0100

    création du component profil et importation dans app

---
+++ exo5/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Profil from './components/Profil';
+
+function App() {
+  return (
+    <div>
+      <Profil/>
+      <Profil/>
+      <Profil/>
+    </div>
+  );
+}
+
+export default App;
---
+++ exo5/src/components/Profil.js
@@ -0,0 +1,15 @@
+import {Component} from "react";
+
+class Profil extends Component {
+    render(){
+    return (
+      <div>
+        <h1>Prenom: Nicolas</h1>
+        <h2>Nom: Primo</h2>
+        <p>Description: un jeune et beau coach puissant (aimez vous guys)</p>
+      </div>
+    );
+  }
+}
+  
+  export default Profil;
