34478e63fa09b4a934971f229daa95dbc7654d7a
Kadir <kadir.poyraz@outlook.be>
2021-01-14 12:51:05 +0100

    fin exercice 3

--- exo3/src/App.js
+++ exo3/src/App.js
@@ -1,8 +1,10 @@
 import Ninja from "./ninja.webp"
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
+      <Navbar/>
       <h1>Exercice 3</h1>
       <img src={Ninja} alt="Ninja"/>
     </div>
