e92778e743c285f4d660a8f048f47265275ec5ef
Kadir <kadir.poyraz@outlook.be>
2021-01-14 12:01:03 +0100

    Partie 2 exo 2

--- exo2/src/App.js
+++ exo2/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
+import logo2 from './user.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <img src={logo} className="App-logo" alt="logo" />
+      <img src={logo2} className="user-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
